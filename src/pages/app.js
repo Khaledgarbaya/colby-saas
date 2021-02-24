d70656339fe8506fbd16e2163b57b82aeeccd348
Khaled Garbaya <kgarbaya@gmail.com>
2021-02-24 20:58:06 +0100

    first commit

---
+++ src/pages/app.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Router} from '@reach/router'
+
+const Dashboard = () => <h1>Dashboard</h1>
+const Default = () => <h1>Main App</h1>
+const App = () => {
+  return (
+    <Router basepath="/app">
+      <Dashboard path="/dasboard" />
+      <Default path="/" />
+    </Router>
+  )
+}
+
+export default App
